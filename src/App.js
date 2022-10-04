1bf6df1d403a8617d8ce5a8005f7b282e362ff17
Level <gringapara163@gmail.com>
2022-10-04 16:02:44 -0300

    alteration

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
 
 function App() {
+  const name = 'Dagoberto'
+
+
+
+function sum(a,b){
+  return a + b
+}
+
+const url = "https://via.placeholder.com/150"
+
+  const newname = name.toUpperCase()
   return (
+    
     <div className="App">
-      <h1>Meu Projeto</h1>
-      <p>Meu Primeiro App</p>
+      <h2>Alterando o JSX</h2>
+      <p>olá, {newname}</p>
+      <p>Soma: {sum(1,2)}</p>
+      <img src ={url} alt="minha imagem" />
     </div>
   );
 }
